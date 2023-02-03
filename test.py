c10908d58adb46cf0ceed9f56b782d21d12e5813
educia27 <edd1114@gmail.com>
2023-02-03 05:21:22 -0600

    classes

--- test.py
+++ test.py
@@ -277,3 +277,18 @@ y = itertools.repeat('hello', 3)
 print(list(y))
 
 print("-----------------------")
+
+class SimpleGradebook:
+
+ def __init__(self):
+    self._grades = {}
+
+ def add_student(self, name):
+    self._grades[name] = []
+
+ def report_grade(self, name, score):
+    self._grades[name].append(score)
+
+ def average_grade(self, name):
+    grades = self._grades[name]
+    return sum(grades) / len(grades)
