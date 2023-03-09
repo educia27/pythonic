0ed3522ec824cb795ae858a2d4a34f73c46a3f36
educia27 <edd1114@gmail.com>
2023-03-09 05:37:30 -0600

    enumeration

---
+++ enumeration.py
@@ -0,0 +1,14 @@
+
+names = ['eddy','sarah','mike','frank','tony']
+
+# counter = 0 
+
+# for name in names:
+#     print(f'{counter}' : {name})
+#     counter +=1
+
+for index, name in enumerate(names):
+    print(f'{index} : {name}')
+
+
+print(list(enumerate(names)))
