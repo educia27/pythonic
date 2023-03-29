a6e6144d9b479c94f762a81bf2ca799a2bb2964f
educia27 <edd1114@gmail.com>
2023-03-29 10:14:15 -0500

    unittesting

--- unittesting/student.py
+++ unittesting/student.py
@@ -1,3 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('num1', help="int to add", type= int)
+parser.add_argument('num2', help="2nd number to add", type= int)
+args = parser.parse_args()
+
 class Student:
     
     stipend_hike_rate = 1.05
@@ -22,3 +29,16 @@ class Student:
     def sayHello(self):
         return (f'Hello {self.first}!')
     
+    def __len__(self):
+        return len(self.first) + len(self.last)
+
+ 
+    
+def adding(num1,num2):
+    return num1 + num2
+ 
+person1 = Student("Frank", "Riberyyyyy",12000)
+
+
+if __name__=="__main__":
+    print(adding(args.num1, args.num2))
